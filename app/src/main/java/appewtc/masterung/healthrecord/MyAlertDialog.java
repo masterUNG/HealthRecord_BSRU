d342d61770f16f1585ca908e0469391c80f2684a
masterUNG <phrombutr@gmail.com>
2016-01-18 15:17:15 +0700

    Create MyAlertDialog finish

---
+++ app/src/main/java/appewtc/masterung/healthrecord/MyAlertDialog.java
@@ -0,0 +1,29 @@
+package appewtc.masterung.healthrecord;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by masterUNG on 1/18/16 AD.
+ */
+public class MyAlertDialog {
+
+    public void myDialog(Context context, int intIcon, String strTitle, String strMessage) {
+
+        AlertDialog.Builder objBuilder = new AlertDialog.Builder(context);
+        objBuilder.setIcon(intIcon);
+        objBuilder.setTitle(strTitle);
+        objBuilder.setMessage(strMessage);
+        objBuilder.setCancelable(false);
+        objBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        objBuilder.show();
+
+    }   // myDialog
+
+}   // Main Class
